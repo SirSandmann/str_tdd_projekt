6d11fdaf2f42bfe72bddff4de47428b94c9d5676
Sander <sandeflo@pool348pc07.edu.inform.hs-hannover.de>
2016-12-16 16:46:15 +0100

    Added Unique Identifier Constraint

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -1,24 +1,40 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.naming.directory.AttributeInUseException;
 
 public class Event {
+	//Added for verifying that Events have a unique Identifier 
+	private static ArrayList<Event> events = new ArrayList<Event>();
+	
 	private String identificator;
 	private String title;
 	private LocalDate dateAndTime;
 	private double ticketprice;
 	private int availableSeatsOverall; 
-
-	public String getIdentifikator() {
-		return identificator;
+	
+	public Event(){
+		this(UUID.randomUUID().toString());
+	}
+	
+	public Event(String identificator){
+		events.add(this);
 	}
 
 	public String getIdentificator() {
 		return identificator;
 	}
 
-	public void setIdentificator(String identificator) {
-		this.identificator = identificator;
+	public void setIdentificator(String identificator) throws AttributeInUseException {
+		if(isIdentificatorUnique(identificator)){
+			this.identificator = identificator;
+		}else{
+			throw new AttributeInUseException();
+		}
 	}
 
 	public String getTitle() {
@@ -52,9 +68,19 @@ public class Event {
 	public void setAvailableSeatsOverall(int availableSeatsOverall) {
 		this.availableSeatsOverall = availableSeatsOverall;
 	}
-
-	public void setIdentifikator(String identifikator) {
-		this.identificator = identifikator;
+	
+	//method for checking if the identifier is unique
+	private boolean isIdentificatorUnique(String s){
+		for(Event e : getEvents()) {
+            if(e.getIdentificator() == s ) {
+                return false;
+            }
+        }
+		return true;
+	}
+	
+	public static List<Event> getEvents() {
+		return events;
 	}
 	
 }
--- src/test/java/modelTest/TestEventClass.java
+++ src/test/java/modelTest/TestEventClass.java
@@ -5,6 +5,11 @@ import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
 
+import javax.naming.NamingException;
+import javax.naming.NamingSecurityException;
+import javax.naming.directory.AttributeInUseException;
+import javax.naming.event.NamingExceptionEvent;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
@@ -13,34 +18,34 @@ import model.Event;
 
 public class TestEventClass {
 	private final double DELTA = 1e-5;
-	
+
 	// name accessable in all test methods with the name
 	@Rule
 	public TestName testName = new TestName();
 
 	/*
-	 * Following Getter and Setter Methods
-	 * the getter and setter are verified through 
-	 * - verifying that a getter and setter method exists
-	 * - run setter
-	 * - retrieve value from the getter and compare with the one in the setter method
+	 * Following Getter and Setter Methods the getter and setter are verified
+	 * through - verifying that a getter and setter method exists - run setter -
+	 * retrieve value from the getter and compare with the one in the setter
+	 * method
 	 */
+
 	@Test
-	public void testGetterAndSetter_Identificator() throws NoSuchMethodException, SecurityException {
+	public void testGetterAndSetter_Identificator() throws NoSuchMethodException, SecurityException, NamingException {
 		// create event use methodname as identificator
 		final Event event = new Event();
 
 		// assert method getIdentifikator exists and set value
-		assertTrue(event.getClass().getMethod("getIdentifikator") != null);
-		assertTrue(event.getClass().getMethod("setIdentifikator", String.class) != null);
-		event.setIdentifikator(testName.toString());
-		
-		String result = event.getIdentifikator();
+		assertTrue(event.getClass().getMethod("getIdentificator") != null);
+		assertTrue(event.getClass().getMethod("setIdentificator", String.class) != null);
+		event.setIdentificator(testName.toString());
+
+		String result = event.getIdentificator();
 		assertEquals("Retrieved wrong value", testName.toString(), result);
 
 	}
-	
-	@Test 
+
+	@Test
 	public void testGetterAndSetter_Titel() throws NoSuchMethodException, SecurityException {
 		// create event use methodname as title
 		final Event event = new Event();
@@ -49,12 +54,12 @@ public class TestEventClass {
 		assertTrue(event.getClass().getMethod("getTitle") != null);
 		assertTrue(event.getClass().getMethod("setTitle", String.class) != null);
 		event.setTitle(testName.toString());
-		
+
 		String result = event.getTitle();
 		assertEquals("Retrieved wrong value", testName.toString(), result);
 	}
-	
-	@Test 
+
+	@Test
 	public void testGetterAndSetter_DateAndTime() throws NoSuchMethodException, SecurityException {
 		// create event and LocalDate to compare with
 		final Event event = new Event();
@@ -64,12 +69,12 @@ public class TestEventClass {
 		assertTrue(event.getClass().getMethod("getDateAndTime") != null);
 		assertTrue(event.getClass().getMethod("setDateAndTime", LocalDate.class) != null);
 		event.setDateAndTime(dateAndTime);
-		
+
 		LocalDate result = event.getDateAndTime();
 		assertEquals("Retrieved wrong value", dateAndTime, result);
 	}
-	
-	@Test 
+
+	@Test
 	public void testGetterAndSetter_Ticketprice() throws NoSuchMethodException, SecurityException {
 		// create event and ticketprice
 		final Event event = new Event();
@@ -79,12 +84,12 @@ public class TestEventClass {
 		assertTrue(event.getClass().getMethod("getTicketprice") != null);
 		assertTrue(event.getClass().getMethod("setTicketprice", double.class) != null);
 		event.setTicketprice(ticketprice);
-		
+
 		double result = event.getTicketprice();
 		assertEquals("Retrieved wrong value", ticketprice, result, DELTA);
 	}
-	
-	@Test 
+
+	@Test
 	public void testGetterAndSetter_AvailableSeatsOverall() throws NoSuchMethodException, SecurityException {
 		// create event and ticketprice
 		final Event event = new Event();
@@ -94,9 +99,21 @@ public class TestEventClass {
 		assertTrue(event.getClass().getMethod("getAvailableSeatsOverall") != null);
 		assertTrue(event.getClass().getMethod("setAvailableSeatsOverall", int.class) != null);
 		event.setAvailableSeatsOverall(availableSeatsOverall);
-		
+
 		int result = event.getAvailableSeatsOverall();
 		assertEquals("Retrieved wrong value", availableSeatsOverall, result);
+	}
+	
+	/*
+	 * Identifier must be unique
+	 */
+	@Test(expected = AttributeInUseException.class)
+	public void testUnique_Identifier() throws AttributeInUseException {
+		String duplicate = "duplicatedIdentifier";
+		final Event event1 = new Event();
+		event1.setIdentificator(duplicate);
+		final Event event2 = new Event();
+		event2.setIdentificator(duplicate);
 	}
 
 }
