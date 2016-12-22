624b791b53b93c56197c6da6ab341dc1b6c22921
sirsandmann <florian@sander-net.de>
2016-12-22 20:00:52 +0100

    Modified Testsuite and Added Create Reservation test

--- src/test/java/serviceTest/AddInstancesTest.java
+++ src/test/java/serviceTest/AddInstancesTest.java
@@ -42,5 +42,16 @@ public class AddInstancesTest {
 		
 		assertArrayEquals("Not complete List retrieved", referenceList.toArray(), Event.getEvents().toArray());
 	}
+	
+	@Test
+	public void testCreateReservation() throws EventSameDateAndTitleException, NotUniqueIdentifierException, CustomerSameNameException {
+		ArrayList<Reservation> referenceList= new ArrayList<Reservation>();
+		Event event = new Event("EventTitle",LocalDate.now(),59.99, 10000);
+		Customer customer = new Customer("Peter CustName", "Ricklinger Stadtweg 120");
+		Reservation reservation = new Reservation(customer, event, 5000);
+		referenceList.add(reservation);
+		
+		assertArrayEquals("Not complete List retrieved", referenceList.toArray(), Reservation.getReservations().toArray());
+	}
 
 }
