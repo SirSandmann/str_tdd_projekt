8e39cc54ae9d4863c21ac5f0c2e06ba6f15623d6
Sascha Becker <s.becker@wertarbyte.com>
2016-12-28 12:43:15 +0100

    Remove not needed customer tests in AddInstancesTest class.

--- src/test/java/serviceTest/AddInstancesTest.java
+++ src/test/java/serviceTest/AddInstancesTest.java
@@ -16,42 +16,32 @@ import model.Event;
 import model.Reservation;
 
 public class AddInstancesTest {
-	
-	@Before
-	public void clearAllLists() {
-		Reservation.clearReservations();
-		//Customer.clearAllCustomers();
-		Event.clearAllEvents();
-		
-	}
-	
-	@Test
-	public void testCreateCustomer() throws CustomerSameNameException{
-		ArrayList<Customer> referenceList= new ArrayList<Customer>();
-		Customer customer = new Customer("Peter CustName", "Ricklinger Stadtweg 120");
-		referenceList.add(customer);
-		
-		//assertArrayEquals("Not complete List retrieved", referenceList.toArray(), Customer.getCustomers().toArray());
-	}
-	
-	@Test
-	public void testCreateEvent() throws EventSameDateAndTitleException, NotUniqueIdentifierException {
-		ArrayList<Event> referenceList= new ArrayList<Event>();
-		Event event = new Event("EventTitle",LocalDate.now(),59.99, 10000);
-		referenceList.add(event);
-		
-		assertArrayEquals("Not complete List retrieved", referenceList.toArray(), Event.getEvents().toArray());
-	}
-	
-	@Test
-	public void testCreateReservation() throws EventSameDateAndTitleException, NotUniqueIdentifierException, CustomerSameNameException {
-		ArrayList<Reservation> referenceList= new ArrayList<Reservation>();
-		Event event = new Event("EventTitle",LocalDate.now(),59.99, 10000);
-		Customer customer = new Customer("Peter CustName", "Ricklinger Stadtweg 120");
-		Reservation reservation = new Reservation(customer, event, 5000);
-		referenceList.add(reservation);
-		
-		assertArrayEquals("Not complete List retrieved", referenceList.toArray(), Reservation.getReservations().toArray());
-	}
+
+    @Before
+    public void clearAllLists() {
+        Reservation.clearReservations();
+        Event.clearAllEvents();
+
+    }
+
+    @Test
+    public void testCreateEvent() throws EventSameDateAndTitleException, NotUniqueIdentifierException {
+        ArrayList<Event> referenceList = new ArrayList<Event>();
+        Event event = new Event("EventTitle", LocalDate.now(), 59.99, 10000);
+        referenceList.add(event);
+
+        assertArrayEquals("Not complete List retrieved", referenceList.toArray(), Event.getEvents().toArray());
+    }
+
+    @Test
+    public void testCreateReservation() throws EventSameDateAndTitleException, NotUniqueIdentifierException, CustomerSameNameException {
+        ArrayList<Reservation> referenceList = new ArrayList<Reservation>();
+        Event event = new Event("EventTitle", LocalDate.now(), 59.99, 10000);
+        Customer customer = new Customer("Peter CustName", "Ricklinger Stadtweg 120");
+        Reservation reservation = new Reservation(customer, event, 5000);
+        referenceList.add(reservation);
+
+        assertArrayEquals("Not complete List retrieved", referenceList.toArray(), Reservation.getReservations().toArray());
+    }
 
 }
