834cc72bb37234bfb0eaae6a5103bfa62aef4051
Sascha Becker <s.becker@wertarbyte.com>
2016-12-28 14:17:35 +0100

    Add persistence for reservations in service class.

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -7,10 +7,12 @@ import custom_exceptions.CustomerSameNameException;
 import custom_exceptions.NotUniqueIdentifierException;
 import model.Customer;
 import model.Event;
+import model.Reservation;
 
 public class Service {
     private static HashMap<String, Customer> customers = new HashMap<String, Customer>();
     private static HashMap<UUID, Event> events = new HashMap<UUID, Event>();
+    private static HashMap<UUID, Reservation> reservations = new HashMap<UUID, Reservation>();
 
     public static void addCustomer(Customer c) throws CustomerSameNameException {
         if (!customers.containsKey(c.getName())) {
@@ -28,7 +30,16 @@ public class Service {
         }
     }
 
+    public static void addReservation(Reservation r) throws NotUniqueIdentifierException {
+        if (!reservations.containsKey(r.getUuid())) {
+            reservations.put(r.getUuid(), r);
+        } else {
+            throw new NotUniqueIdentifierException();
+        }
+    }
+
     public static void reset() {
+        resetReservations();
         resetEvents();
         resetCustomers();
     }
@@ -41,11 +52,19 @@ public class Service {
         events.clear();
     }
 
+    private static void resetReservations() {
+        reservations.clear();
+    }
+
     public static HashMap<String, Customer> getCustomers() {
         return customers;
     }
 
     public static HashMap<UUID, Event> getEvents() {
         return events;
+    }
+
+    public static HashMap<UUID, Reservation> getReservations() {
+        return reservations;
     }
 }
--- src/test/java/serviceTest/ServiceTest.java
+++ src/test/java/serviceTest/ServiceTest.java
@@ -4,6 +4,7 @@ import custom_exceptions.CustomerSameNameException;
 import custom_exceptions.NotUniqueIdentifierException;
 import model.Customer;
 import model.Event;
+import model.Reservation;
 import org.junit.Before;
 import org.junit.Test;
 import service.Service;
@@ -24,6 +25,8 @@ public class ServiceTest {
     public void addCustomer() throws CustomerSameNameException {
         String name = "Klaus";
         String address = "Musterstraße 1a";
+
+        assertEquals("Collection must be empty", 0, Service.getCustomers().size());
 
         Customer c = new Customer(name, address);
         Service.addCustomer(c);
@@ -46,11 +49,33 @@ public class ServiceTest {
         Event e = new Event(name, new Date(), price, freeSeats);
         Service.addEvent(e);
 
-        assertEquals("Odd number of elements in customers collection", 1, Service.getEvents().size());
+        assertEquals("Odd number of elements in events collection", 1, Service.getEvents().size());
 
         Event eGet = Service.getEvents().get(e.getUuid());
 
         assertTrue("Added customer is not correct persisted", e.equals(eGet));
+    }
+
+    @Test
+    public void addReservation() throws NotUniqueIdentifierException, CustomerSameNameException {
+        Event e = new Event("König der Löwen", new Date(),  129.99, 2000);
+        Service.addEvent(e);
+        assertEquals("Odd number of elements in events collection", 1, Service.getEvents().size());
+        Event eGet = Service.getEvents().get(e.getUuid());
+
+        Customer c = new Customer("Klaus", "Musterstraße 1a");
+        Service.addCustomer(c);
+        assertEquals("Odd number of elements in customers collection", 1, Service.getCustomers().size());
+        Customer cGet = Service.getCustomers().get(c.getName());
+
+        assertEquals("Collection must be empty", 0, Service.getReservations().size());
+        Reservation r = new Reservation(cGet.getName(), eGet.getUuid(), 3);
+        Service.addReservation(r);
+        assertEquals("Odd number of elements in reservations collection", 1, Service.getReservations().size());
+
+        Reservation rGet = Service.getReservations().get(r.getUuid());
+
+        assertTrue("Added customer is not correct persisted", r.equals(rGet));
     }
 
     @Test(expected = CustomerSameNameException.class)
