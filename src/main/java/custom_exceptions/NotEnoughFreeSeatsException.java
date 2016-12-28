04a2836bc406ada281106ea673ad656c2347bb35
Sascha Becker <s.becker@wertarbyte.com>
2016-12-28 19:29:52 +0100

    Removed unused exceptions.

--- src/main/java/custom_exceptions/NotEnoughFreeSeatsException.java
+++ src/main/java/custom_exceptions/NotEnoughFreeSeatsException.java
@@ -1,21 +1,9 @@
 package custom_exceptions;
 
 public class NotEnoughFreeSeatsException extends Exception{
-
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public NotEnoughFreeSeatsException() {
         super();
-    }
-
-	public NotEnoughFreeSeatsException(String message) {
-        super(message);
-    }
-
-    public NotEnoughFreeSeatsException(String message, Throwable throwable) {
-        super(message, throwable);
     }
 }
--- src/main/java/custom_exceptions/NotUniqueIdentifierException.java
+++ src/main/java/custom_exceptions/NotUniqueIdentifierException.java
@@ -1,21 +1,9 @@
 package custom_exceptions;
 
 public class NotUniqueIdentifierException extends Exception{
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public NotUniqueIdentifierException() {
         super();
-    }
-	
-	public NotUniqueIdentifierException(String message) {
-        super(message);
-    }
-
-    public NotUniqueIdentifierException(String message, Throwable throwable) {
-        super(message, throwable);
     }
 }
